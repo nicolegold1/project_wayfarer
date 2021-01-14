af905c0e4ae4718e116f253bac0f44c8183f94aa
nikkigtech <negoldson@yahoo.com>
2021-01-14 17:25:22 -0500

    pre merge

--- main_app/views.py
+++ main_app/views.py
@@ -8,4 +8,21 @@ def profile(req):
 
 
 def homepage(request):
-    return render(request, 'homepage.html')
+     error_message = ''
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('wayfarer_index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    # A GET or bad POST request, renders empty form
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'homepage.html', context)
+
+  
+
+
+   

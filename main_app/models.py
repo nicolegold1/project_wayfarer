ccf17724c47c50507dc31ae9494c5de1e79a63e8
smadzelonka <smadzelonka@gmail.com>
2021-01-14 13:53:44 -0800

    basic models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class City(models.Model):
+    pass
+
+
+class Post(models.Model):
+    pass

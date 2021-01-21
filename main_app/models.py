42d59b484add8c7ba39a4f65845e6f5aa9d2b76f
victordoyle <victor-doyle@outlook.com>
2021-01-21 13:34:37 -0500

    pre merge save

--- main_app/models.py
+++ main_app/models.py
@@ -26,7 +26,7 @@ class City(models.Model):
     flags = models.CharField(max_length=200)
     user = models.ForeignKey(Account, on_delete=models.CASCADE)
     """ posts = models.ManyToManyField(Post, blank=True)  """
-    slug = models.SlugField(default='', editable=False, max_length=20, null=True)
+    slug = models.SlugField(default='', editable=False, max_length=20, null=False)
 
 
     def get_absolute_url(self):

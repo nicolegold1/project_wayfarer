9bd06d0aa981af9d80b04d4cd025867aa20a1d3f
smadzelonka <smadzelonka@gmail.com>
2021-01-21 19:03:50 -0800

    more heroku

--- project_wayfarer/settings.py
+++ project_wayfarer/settings.py
@@ -19,11 +19,11 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# EMAIL_HOST = EMAIL_HOST
-# EMAIL_PORT = EMAIL_PORT
-# EMAIL_HOST_USER = EMAIL_HOST_USER
-# EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
-# EMAIL_USE_TLS = EMAIL_USE_TLS
+EMAIL_HOST = EMAIL_HOST
+EMAIL_PORT = EMAIL_PORT
+EMAIL_HOST_USER = EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
+EMAIL_USE_TLS = EMAIL_USE_TLS
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/

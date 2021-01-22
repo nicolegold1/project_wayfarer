1b43378c3461ab8f8cd8e353d693699c3bb052a0
victordoyle <victor-doyle@outlook.com>
2021-01-21 21:53:05 -0500

    edited email

--- project_wayfarer/settings.py
+++ project_wayfarer/settings.py
@@ -18,12 +18,12 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+""" 
 EMAIL_HOST = EMAIL_HOST
 EMAIL_PORT = EMAIL_PORT
 EMAIL_HOST_USER = EMAIL_HOST_USER
 EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
-EMAIL_USE_TLS = EMAIL_USE_TLS
+EMAIL_USE_TLS = EMAIL_USE_TLS """
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
